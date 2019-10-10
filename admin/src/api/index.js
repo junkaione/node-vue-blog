275a73a7375348129ba2d3f819339c7ec302bb3d
张俊凯 <250336272@qq.com>
2019-10-10 10:27:43 +0800

    文章管理布局

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -5,5 +5,10 @@ export default {
   UserPageApi: `${Host}/user/page`, //用户列表
   UserAddApi: `${Host}/user/reg`, //用户新增
   UserEditApi: `${Host}/user/update`, //用户修改
-  UserDelApi: `${Host}/user/del` //用户删除
+  UserDelApi: `${Host}/user/del`, //用户删除
+
+  ArticlePageApi: `${Host}/article/page`, //文章列表
+  ArticleAddApi: `${Host}/article/add`, //文章新增
+  ArticleUpdateApi: `${Host}/article/update`, //文章修改
+  ArticleDelApi: `${Host}/article/del`, //文章删除
 }
--- admin/src/views/Article/index.vue
+++ admin/src/views/Article/index.vue
@@ -1,3 +1,171 @@
 <template>
-  <div>article</div>
+  <div class="article">
+    <el-form ref="searchForm" :model="searchInfo" :inline="true">
+      <el-form-item>
+        <el-input placeholder="文章标题" v-model="searchInfo.title" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="searchInfo.category" placeholder="文章分类" clearable>
+          <el-option label="超级管理员" :value="1"></el-option>
+          <el-option label="普通用户" :value="2"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button icon="el-icon-search" type="primary" @click="getArticleList">搜索</el-button>
+        <el-button icon="el-icon-edit" type="primary" @click="handleAdd">添加</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="_id" label="文章ID"></el-table-column>
+      <el-table-column prop="title" label="文章标题"></el-table-column>
+      <el-table-column label="作者">
+        <template slot-scope="scope">
+          <div>{{scope.row.author.username}}</div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="addTime" label="新增时间"></el-table-column>
+      <el-table-column prop="updatedTime" label="最后更新时间"></el-table-column>
+      <el-table-column prop="commentNum" label="评论量"></el-table-column>
+      <el-table-column prop="viewNum" label="阅读量"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button type="danger" size="small" @click="handleDel(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pagination">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :page-size="searchInfo.pageSize"
+        :total="total"
+        @current-change="handleCurrentChange"
+      ></el-pagination>
+    </div>
+    <el-dialog
+      class="dialog"
+      :title="dialogTitle"
+      :visible.sync="dialogVisible"
+      width="40%"
+      @close="handleClose"
+    >
+      <el-form ref="changeForm" :model="changeInfo" label-width="90px">
+        <el-form-item label="用户名：">
+          <el-input v-model="changeInfo.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码：">
+          <el-input v-model="changeInfo.password" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="用户类型：" v-if="dialogTitle === '修改'">
+          <el-select v-model="changeInfo.type" placeholder="用户类型" clearable>
+            <el-option label="超级管理员" :value="1"></el-option>
+            <el-option label="普通用户" :value="2"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleSure">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
+
+<script>
+import Api from "@/api";
+
+export default {
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      searchInfo: {
+        currentPage: 1,
+        pageSize: 10
+      },
+      changeInfo: {},
+      dialogVisible: false,
+      dialogTitle: "",
+      handleChangeUrl: ""
+    };
+  },
+  mounted() {
+    this.getArticleList();
+  },
+  methods: {
+    getArticleList() {
+      this.$get(Api.ArticlePageApi, this.searchInfo).then(res => {
+        this.tableData = res.data.result;
+        this.total = res.data.total;
+      });
+    },
+    handleAdd() {
+      this.dialogVisible = true;
+      this.dialogTitle = "添加";
+      this.handleChangeUrl = Api.UserAddApi;
+    },
+    handleEdit(row) {
+      this.dialogVisible = true;
+      this.dialogTitle = "修改";
+      this.handleChangeUrl = Api.UserEditApi;
+      this.changeInfo = row;
+    },
+    handleDel(row) {
+      this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(() => {
+        this.$post(Api.UserDelApi, {
+          _id: row._id
+        }).then(res => {
+          if (res.code === "000000") {
+            this.$message({
+              type: "success",
+              message: res.msg
+            });
+            this.getUserList();
+          }
+        });
+      });
+    },
+    handleSure() {
+      this.$post(this.handleChangeUrl, this.changeInfo).then(res => {
+        if (res.code === "000000") {
+          this.dialogVisible = false;
+          this.$message({
+            type: "success",
+            message: res.msg
+          });
+          this.getUserList();
+        }
+      });
+    },
+    handleCurrentChange(val) {
+      this.searchInfo.currentPage = val;
+      this.getUserList();
+    },
+    handleClose() {
+      this.changeInfo = {};
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.article {
+  .pagination {
+    text-align: center;
+    margin-top: 20px;
+  }
+  .dialog {
+    .el-form {
+      padding: 0 50px;
+    }
+    .el-select {
+      width: 100%;
+    }
+  }
+}
+</style>
